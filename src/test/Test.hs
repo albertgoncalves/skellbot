8b37d3182fdf31853531394e6cafd6dddd24cce7
albertgoncalves <goncalves.albert@gmail.com>
2019-05-24 23:14:16 -0400

    revised tests

--- src/test/Test.hs
+++ src/test/Test.hs
@@ -2,11 +2,14 @@
 
 module Main where
 
-import Commands (parse, tokenize)
+import Commands (convert, filterCommands, parse, pipeCommands, tokenize)
+import Data.Map.Strict (keys)
+import Data.Text (intercalate)
+import qualified Data.Text as T
 import Test.HUnit (Counts, Test(TestCase, TestList), (@=?), runTestTT)
 import Test.HUnit.Lang (Assertion)
 import Transport (extract, inject, relay)
-import Types (Response(Websocket), message)
+import Types (Command(Meta, Pipe), Response(Websocket), message)
 
 testExtract :: Assertion
 testExtract = extract input @=? Just (message "id" "hello" "user" "channel")
@@ -20,8 +23,23 @@ testExtract = extract input @=? Just (message "id" "hello" "user" "channel")
 
 testTokenize :: [Assertion]
 testTokenize =
-    [ tokenize "!hello|!flip" @=? [["!hello"], ["!flip"]]
-    , tokenize "!hello | !flip" @=? [["!hello"], ["!flip"]]
+    [ tokenize "!echo hello|!flip" @=? [["!echo", "hello"], ["!flip"]]
+    , tokenize "!echo hello | !flip" @=? [["!echo", "hello"], ["!flip"]]
+    ]
+
+testConvert :: [Assertion]
+testConvert =
+    [ convert ["!help"] @=? Just (Meta ("!help", ""))
+    , convert ["!echo", "hello"] @=? Just (Pipe ("!echo", "hello"))
+    , map convert [["!help"], ["!flip"]] @=?
+      [Just (Meta ("!help", "")), Just (Pipe ("!flip", ""))]
+    ]
+
+testFilterCommands :: [Assertion]
+testFilterCommands =
+    [ filterCommands [Meta ("!help", ""), Pipe ("!flip", "")] @=? Nothing
+    , filterCommands [Pipe ("!echo", "hello"), Pipe ("!flip", "")] @=?
+      Just [Pipe ("!echo", "hello"), Pipe ("!flip", "")]
     ]
 
 testParse :: [Assertion]
@@ -33,18 +51,22 @@ testParse =
       Just "_bernar has been *banned*_ in 2019"
     , parse "!echo bernar | !ban | !flip | !echo foo bar baz" @=? Nothing
     , parse "!echo foo bar baz | !flip" @=? Just "zab rab oof"
-    , parse "!ban | !flip | !echo foo bar baz | !hello" @=? Nothing
+    , parse "!ban | !flip | !echo foo bar baz | !echo hello" @=? Nothing
     , parse "!bernar | !ban | !flip | | !echo foo bar baz" @=? Nothing
     , parse "!bernar | !ban | !flip | !echo foo bar baz |" @=? Nothing
     , parse "!echo hello | !ban | !2019 | !flip" @=?
       Just "9102 ni _*dennab* neeb sah olleh_"
-    , parse "!hello | !ban | !2019 | !flip | !hello" @=? Nothing
     , parse "!echo :stache:" @=? Just ":stache:"
+    , parse "!help" @=? (Just . f . intercalate "`\\n`" . keys) pipeCommands
+    , parse "!help | !flip" @=? Nothing
+    , parse "!echo foo| !flip | !echo bar" @=? Nothing
     ]
+  where
+    f x = T.concat ["`", x, "`"]
 
 testRelay :: [Assertion]
 testRelay =
-    [ relay "A" 1 (message "1" "!hello" "A" "channel") @=? Nothing
+    [ relay "A" 1 (message "1" "!echo hello" "A" "channel") @=? Nothing
     , relay "A" 1 (message "1" "!echo foo | | !upper" "B" "channel") @=?
       Nothing
     , relay "A" 1 (message "1" "!echo {}" "B" "channel") @=? Nothing
@@ -59,4 +81,12 @@ testRelay =
 main :: IO Counts
 main = (runTestTT . TestList . map TestCase) xs
   where
-    xs = testExtract : testTokenize ++ testParse ++ testRelay
+    xs =
+        testExtract :
+        concat
+            [ testTokenize
+            , testConvert
+            , testParse
+            , testFilterCommands
+            , testRelay
+            ]
